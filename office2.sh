e90994776f271364ef3cbf7fc98142655aa11df0
Kasper Lewau <lewauu@gmail.com>
2018-10-08 09:28:06 +0200

    update

---
+++ office2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 5120x360 --rotate normal --output DP1 --off --output DP1-3 --off --output DP1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP2 --off
