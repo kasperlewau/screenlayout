d457cef50db8884a3359c856659600705b0a2c28
Kasper Lewau <lewauu@gmail.com>
2018-10-18 21:34:17 +0200

    add home setup
    update script to handle home, office & mobile

--- swap.sh
+++ swap.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-xrandr | grep "DP-1-1 connected"
-if [[ $? == 0 ]]; then
-	xrandr --output eDP-1 --mode 1920x1080 --pos 5120x360 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-1-3 --off --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
+
+office=`xrandr | grep "^DP-1-1 connected"`
+home=`xrandr | grep "^DP-1 connected"`
+
+if [ ! -z "$office" ]; then
+	./office.sh
+elif [ ! -z "$home" ]; then
+	./home.sh
 else
-	xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
+	./mobile.sh
 fi
-
-
