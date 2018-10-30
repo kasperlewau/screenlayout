a50f6385de34178e6badd3086a9373b3656f3819
Kasper Lewau <lewauu@gmail.com>
2018-10-30 09:17:08 +0100

    fix swap script

--- swap.sh
+++ swap.sh
@@ -4,9 +4,12 @@ office=`xrandr | grep "^DP-1-1 connected"`
 home=`xrandr | grep "^DP-1 connected"`
 
 if [ ! -z "$office" ]; then
-	./office.sh
+	# ./office.sh
+	xrandr --output eDP-1 --mode 1920x1080 --pos 5120x360 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-1-3 --off --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
 elif [ ! -z "$home" ]; then
-	./home.sh
+	# ./home.sh
+	xrandr --output DP-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
 else
-	./mobile.sh
+	# ./mobile.sh
+	xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
 fi
